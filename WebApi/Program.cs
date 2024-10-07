e858b1a52d726d409243c237291376ff36d3b6c3
Sri Gol <sgolyalla@outlook.com>
2024-10-07 15:56:14 -0400

    Renamed TodoDb to TodoDbContext

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // EntityFramework DbContext
-builder.Services.AddDbContext<TodoDb>(opt =>
+builder.Services.AddDbContext<TodoDbContext>(opt =>
 {
     opt.UseInMemoryDatabase("TodoList");
 });
